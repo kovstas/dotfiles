c97b1725f599d4e5731a39745a77e149fb274bb5
PepRoll <peproll@protonmail.com>
2018-05-06 21:01:27 +0300

    guake isn't needed

--- guake-backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gconftool-2 --dump /apps/guake > apps-guake.xml
-gconftool-2 --dump /schemas/apps/guake > schemas-apps-guake.xml
--- guake-restore.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gconftool-2 --load apps-guake.xml
-gconftool-2 --load schemas-apps-guake.xml
