a6aa0bea786a8592901f78ccfa2b61da2cc40567
PepRoll <peproll@protonmail.com>
2018-07-30 10:35:55 +0300

    additions settings for org refile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -664,6 +664,8 @@
 
   (setq org-directory "~/Dropbox/org")
   (setq org-agenda-files '("~/Dropbox/org"))
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+  (setq org-refile-allow-creating-parent-nodes 'confirm)
 
   (let ((default-directory org-directory))
     (setq org-default-notes-file (expand-file-name "refile.org"))
@@ -676,33 +678,27 @@
                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
                 ("h" "REPEAT" entry (file org-default-notes-file)
                  "* REPEAT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: REPEAT\n:END:\n")))))
+
+  ;; Agenda
   (setq org-agenda-dim-blocked-tasks nil)
   (setq org-agenda-compact-blocks t)
-  ;; Resume clocking task when emacs restarted
-  (org-clock-persistence-insinuate)
-  ;; Show lot of clocking history so it's easy to pick items off the C-F11 list
-  (setq org-clock-history-length 23)
-  ;; Resume clocking task on clock-in if the clock is open
-  (setq org-clock-in-resume t)
-  ;; Change tasks to NEXT when clocking in
-  (setq org-clock-in-switch-to-state 'bh/clock-in-to-next)
-  ;; Separate drawers for clocking and logs
-  (setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
-  ;; Save clock data and state changes and notes in the LOGBOOK drawer
-  (setq org-clock-into-drawer t)
-  ;; Sometimes I change tasks I'm clocking quickly - this removes clocked tasks with 0:00 duration
-  (setq org-clock-out-remove-zero-time-clocks t)
-  ;; Clock out when moving task to a done state
-  (setq org-clock-out-when-done t)
-  ;; Save the running clock and all clock history when exiting Emacs, load it on startup
-  (setq org-clock-persist t)
-  ;; Do not prompt to resume an active clock
-  (setq org-clock-persist-query-resume nil)
-  ;; Enable auto clock resolution for finding open clocks
-  (setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
-  ;; Include current clocking task in clock reports
-  (setq org-clock-report-include-clocking-task t)
   (setq org-agenda-start-on-weekday 1)
+
+  ;; Calendar
+  (setq calendar-date-style 'european)
+
+  ;; Clocking
+  (org-clock-persistence-insinuate)
+  (setq org-clock-in-resume t)
+  (setq org-clock-in-switch-to-state "NEXT")
+  (setq org-clock-persist t)
+  (setq org-clock-out-when-done t)
+  (setq org-clock-out-remove-zero-time-clocks t)
+  (setq org-clock-into-drawer t)
+  (setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
+  (setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
+  (setq org-clock-history-length 23)
+  (setq org-clock-report-include-clocking-task t)
   
   )
 
